15ccad731b2bb3a4433231de0d811dcd92142f11
EriKurniawan <eri097604@gmail.com>
2023-11-02 09:53:01 +0700

    part 1

---
+++ config/database.php
@@ -0,0 +1,16 @@
+<?php
+
+function getDatabaseConfig(): array{
+    return [
+        "database" => [
+            "url" => "mysql:host=localhost:3306;dbname=php_login_management_test",
+            "username" => "root",
+            "password" => ""
+        ],
+        "prod" => [
+            "url" => "mysql:host=localhost:3306;dbname=php_login_management",
+            "username" => "root",
+            "password" => ""
+        ]
+    ];
+}
